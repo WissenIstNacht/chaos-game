4b98477f53ba0f2c2976e808d6f27618ccdd30ba
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-01-16 17:11:44 +0100

    first commit

---
+++ web/main.dart
@@ -0,0 +1,16 @@
+import 'dart:html';
+
+import 'sketch.dart';
+
+void main() {
+  var canvas = querySelector('#canvasHolder');
+  Sketch(30.0, canvas).run();
+}
+
+double map(double value, double from_a, double from_b, double to_a, double to_b) {
+  var centered_value = value - from_a;
+  var from_delta = from_b - from_a;
+  var to_delta = to_b - to_a;
+  var ratio = to_delta * centered_value / from_delta;
+  return ratio + to_a;
+}
---
+++ web/sketch.dart
@@ -0,0 +1,138 @@
+import 'dart:html';
+
+import 'dart:math';
+
+class Sketch {
+  num _lastTimeStamp = 0;
+  num GAME_SPEED;
+  CanvasElement canvas;
+  CanvasRenderingContext2D ctx;
+
+  List<Point> triangle;
+  Point curr_loc, prev_loc, rand_vertex;
+
+  Random rand = Random();
+
+  AnimationPhase animation_phase = AnimationPhase.curr_location;
+
+  Sketch(double framerate, this.canvas) {
+    GAME_SPEED = 1000 / framerate;
+
+    ctx = canvas.getContext('2d');
+    canvas.height = 400;
+    canvas.width = 600;
+
+    //draw background
+    ctx
+      ..scale(1, -1)
+      ..translate(0, -canvas.height)
+      ..lineWidth = 2
+      ..setFillColorRgb(220, 220, 220)
+      ..fillRect(0, 0, canvas.width, canvas.height)
+      ..strokeRect(0, 0, canvas.width, canvas.height);
+
+    //Define triangle vertices
+    var m = Point(canvas.width / 2, canvas.height / 2);
+
+    var h_offset = sin(pi / 3) * canvas.height / 3;
+    var v_offset = cos(pi / 3) * canvas.height / 3;
+
+    triangle = [
+      Point(m.x, m.y + canvas.height / 3),
+      Point(m.x - h_offset, m.y - v_offset),
+      Point(m.x + h_offset, m.y - v_offset)
+    ];
+
+    //Draw triangle vertices
+    ctx.setFillColorRgb(0, 0, 0);
+    triangle.forEach((Point p) {
+      ctx
+        ..beginPath()
+        ..arc(p.x, p.y, 2, 0, 2 * pi)
+        ..closePath()
+        ..fill();
+    });
+
+    curr_loc = m;
+    prev_loc = Point(-100, -100); //instantiate to location outside canvas.
+  }
+
+  Future run() async {
+    update(await window.animationFrame);
+  }
+
+  void update(num delta) {
+    final diff = delta - _lastTimeStamp;
+
+    if (diff > GAME_SPEED) {
+      _lastTimeStamp = delta;
+      drawFrame();
+    }
+    run();
+  }
+
+  void drawFrame() {
+    switch (animation_phase) {
+      case AnimationPhase.curr_location:
+        //triangle vertices black, prev location black, current location blue
+        ctx.setFillColorRgb(0, 0, 0);
+
+        triangle.forEach((Point p) {
+          ctx
+            ..beginPath()
+            ..arc(p.x, p.y, 4, 0, 2 * pi)
+            ..closePath()
+            ..fill();
+        });
+        ctx
+          ..beginPath()
+          ..arc(prev_loc.x, prev_loc.y, 2, 0, 2 * pi)
+          ..closePath()
+          ..fill();
+        ctx
+          ..setFillColorRgb(50, 50, 255)
+          ..beginPath()
+          ..arc(curr_loc.x, curr_loc.y, 2, 0, 2 * pi)
+          ..closePath()
+          ..fill();
+        animation_phase = AnimationPhase.random_vertex;
+        break;
+      case AnimationPhase.random_vertex:
+        //selected vertex green
+        rand_vertex = triangle[rand.nextInt(3)];
+        ctx
+          ..fillStyle = 'forestGreen'
+          ..beginPath()
+          ..arc(rand_vertex.x, rand_vertex.y, 4, 0, 2 * pi)
+          ..closePath()
+          ..fill();
+        animation_phase = AnimationPhase.new_location;
+        break;
+      case AnimationPhase.new_location:
+        //new location red
+        prev_loc = curr_loc;
+        var average_x = (rand_vertex.x + curr_loc.x) / 2;
+        var average_y = (rand_vertex.y + curr_loc.y) / 2;
+        curr_loc = Point(average_x, average_y);
+        ctx
+          ..fillStyle = '#8B2222'
+          ..beginPath()
+          ..arc(curr_loc.x, curr_loc.y, 2, 0, 2 * pi)
+          ..closePath()
+          ..fill();
+        animation_phase = AnimationPhase.curr_location;
+        break;
+      default:
+    }
+  }
+
+  void _clearCanvas() {
+    ctx
+      ..fillStyle = 'white'
+      ..fillRect(0, 0, canvas.width, canvas.height)
+      ..strokeStyle = 'black'
+      ..strokeRect(0, 0, canvas.width, canvas.height);
+  }
+}
+
+enum AnimationPhase { curr_location, random_vertex, new_location }
